030a3c0d8e154d19fdc1bd860fcacb7b4e83adb8
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-27 21:33:17 +0530

    Add new pages and update .gitignore

---
+++ pages/events/[...slug].js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DynamicEvents() {
+  return <div>Dynamic Events</div>;
+}
---
+++ pages/events/[eventid].js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SpecificEvent() {
+  return <div>Specific Event</div>;
+}
